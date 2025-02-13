b8e619291ab2d0baee2f76aba8242243d0a39adc
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-02-13 20:01:05 +0530

    Add blog-related endpoints to the FastAPI application

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {'data':{'name': 'Dwaipayan'}}
+    return {'data': 'blog list'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+
+@app.get('/blog/{id}')
+def fetch(id: int): #define type
+    return {'data': id}    
+
+
+@app.get('/blog/{id}/comments')
+def fetch_comments(id):
+    return {'data': {'1','2'}}
